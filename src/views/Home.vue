e09214f7e9aa5b5722ea9ef08b8b599501931829
gongjh0926@foxmail.com <gongjh0926@foxmail.com>
2021-03-18 11:01:21 +0800

    topnav样式更新

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,11 +1,15 @@
 <template>
-    <div>
-     <div class="topnav">
-         <div class="logo"></div>
-         <div class="menu"></div>
-     </div>
-     <div class="banner">
-         <h1>轮子UI</h1>
+  <div>
+    <div class="topnav">
+      
+      <div class="logo">LOGO</div>
+      <ul class="menu">
+        <li>菜单1</li>
+        <li>菜单2</li>
+      </ul>
+    </div>
+    <div class="banner">
+      <h1>轮子UI</h1>
          <h2>一个厉害的UI框架</h2>
          <p class="actions">
              <a>GitHub</a>
@@ -15,5 +19,21 @@
     </div>
 </template>
 <style lang="scss" scoped>
-    .topnar{border:1px soild red;}
+.topnav {
+  background: pink;
+  display: flex;
+  padding: 16px;
+  > .logo {
+    max-width: 6em;
+    margin-right: auto;
+  }
+  > .menu {
+    display: flex;
+    white-space: nowrap;
+    flex-wrap: nowrap;
+    > li {
+      margin: 0 1em;
+    }
+  }
+}
 </style>
