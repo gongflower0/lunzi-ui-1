b331d86da545e5e6a9d4d1e6faed7e5b43f5d7e8
gongjh0926@foxmail.com <gongjh0926@foxmail.com>
2021-03-26 15:17:15 +0800

    完善aside逻辑

--- src/components/Topnav.vue
+++ src/components/Topnav.vue
@@ -6,6 +6,7 @@
         <li>菜单1</li>
         <li>菜单2</li>
       </ul>
+      <span class="toggleAside" @click="toggleMenu"></span>
     </div>
 
 </template>
@@ -28,6 +29,8 @@ return {toggleMenu}
   padding: 16px;
   position: relative;
   z-index:10;
+  justify-content: center;
+  align-items:center ;
   > .logo {
     max-width: 6em;
     margin-right: auto;
@@ -39,6 +42,22 @@ return {toggleMenu}
     > li {
       margin: 0 1em;
     }
+  }
+  > .toggleAside{
+   
+    width:24px;
+    height:24px;
+    background: red;
+    position: absolute;
+    left:10px;
+    top:25px;
+    transform:translateY(-50%);
+    display: none;
+  }
+  @media (max-width:500px){
+    > .menu{display:none;}
+    > .logo{margin:0 auto;}
+    > .toggleAside{ display:inline-block;}
   }
 }
 </style>
