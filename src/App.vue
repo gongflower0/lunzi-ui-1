03a0292ef2d0735aaad331a32335359db9e0aeaa
gongjh0926@foxmail.com <gongjh0926@foxmail.com>
2021-03-26 15:38:15 +0800

    路由切换之后关闭aside

--- src/App.vue
+++ src/App.vue
@@ -5,6 +5,7 @@
 
 <script lang="ts">
 import { provide, ref } from 'vue'
+import {router}from './router'
 
 
 export default {
@@ -13,6 +14,11 @@ export default {
     const width=document.documentElement.clientWidth
     const menuVisible=ref(width <=500?false:true);
     provide('menuVisible',menuVisible)
+    router.afterEach(()=>{
+      if(width<=500){
+        menuVisible.value=false;
+      }
+    })
   }
 }
 </script>
